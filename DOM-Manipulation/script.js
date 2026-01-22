ac0a26f6f6a43eae4ccb8db593fbe3ed1c32c63a
shanmukharani <shanmukharani333@gmail.com>
2026-01-22 21:43:37 +0530

    DOM basics: element selection methods practice

---
+++ DOM-Manipulation/script.js
@@ -0,0 +1,14 @@
+// to access a specific HTML element by Id so JavaScript can manipulate it.
+document.getElementById("fpara")
+document.getElementById("fheading")
+
+// to access a specific HTML element by ClassName so JavaScript can manipulate it.
+document.getElementsByClassName("textmatter")
+
+//to access a specific HTML element by TagName so JavaScript can manipulate it.
+document.getElementsByTagName("p")
+
+//
+document.querySelector('#fpara') //Id
+document.querySelector('.textmatter') //ClassName
+document.querySelector('p') //tags
