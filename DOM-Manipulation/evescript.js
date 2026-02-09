d0888b0179c6b19474bf0ee4e87656cff5f3be1f
shanmukharani <shanmukharani333@gmail.com>
2026-02-09 21:41:22 +0530

    events and listener

---
+++ DOM-Manipulation/evescript.js
@@ -0,0 +1,14 @@
+function changeText(event) {
+    console.log(event);
+    let fpara = document.getElementById('fpara');
+    fpara.textContent = "hello world";
+}
+
+let fpara = document.getElementById('fpara');
+
+//adding a event listener
+fpara.addEventListener('click',changeText); // on clicking "hello dhuniya!!!!" it changes to "hello world"
+
+
+//removing a event listener
+//fpara.removeEventListener('click',changeText);
